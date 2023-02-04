c57e36811214c5cc8177f953c105620952fd158c
Rohit Tokala <rtokala@purdue.edu>
2023-02-04 14:26:11 -0500

    Display user name on homepage if logged in

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,3 +1,11 @@
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../lib/firebase";
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  const [user] = useAuthState(auth);
+  return (
+    <div className=" h-[90vh]">
+      {user ? `Logged in as ${user.displayName}` : "User is not logged in"}
+    </div>
+  );
 }
