cf9c874b9abdbf81c3e1f72b0ed7b23694eef1f4
Rohit Tokala <rtokala@purdue.edu>
2023-02-04 14:21:24 -0500

    Add login page layout and buttons

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 export function BaseLayout() {
   return (
-    <div>
+    <div className=" flex flex-col items-center justify-center">
       <NavBar />
       <Outlet />
     </div>
--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -1,3 +1,33 @@
+import { Button } from "../components/ui/button";
+import { auth, signInWithGooglePopup } from "../lib/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  const [user] = useAuthState(auth);
+
+  const navigate = useNavigate();
+  if (user) {
+    navigate("/");
+  }
+  return (
+    <div className=" flex flex-col w-full items-center h-[90vh] justify-center ">
+      <h2 className=" text-2xl font-bold mb-5">Login</h2>
+      <div className=" border border-slate-200 dark:border-slate-700 rounded-xl p-4 flex flex-col items-center gap-4">
+        <Button
+          variant={"default"}
+          className=" w-full"
+          onClick={signInWithGooglePopup}
+        >
+          Login with Google
+        </Button>
+        <Button variant={"default"} className=" w-full">
+          Login with GitHub (TODO)
+        </Button>
+        <Button variant={"default"} className=" w-full">
+          Login with Discord (TODO)
+        </Button>
+      </div>
+    </div>
+  );
 }
