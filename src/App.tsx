748a186cae8837828af86e455e604a52780914d5
Rohit Tokala <rtokala@purdue.edu>
2023-02-04 02:15:14 -0500

    Add dark mode dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { ChevronDown, Laptop, Moon, Sun } from "lucide-react";
 import {
   DropdownMenu,
+  DropdownMenuCheckboxItem,
   DropdownMenuContent,
-  DropdownMenuItem,
   DropdownMenuTrigger,
 } from "./components/ui/dropdown-menu";
 import { Button } from "./components/ui/button";
 
+import { darkTheme } from "./lib/themer";
+
 function App() {
+  const { theme, setTheme } = darkTheme();
   return (
-    <div className=" min-h-screen flex flex-col w-full">
+    <div className="flex flex-col w-full ">
       {/* NavBar */}
-      <div className=" py-2 px-1 items-center justify-evenly w-full flex border border-b-black">
+      <div className=" py-2 px-1 items-center justify-evenly w-full flex border-b border-b-slate-200 dark:border-b-slate-700">
         <div className=" text-xl font-medium">Note-ify</div>
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
@@ -21,18 +24,20 @@ function App() {
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent>
-            <DropdownMenuItem>
+            <DropdownMenuCheckboxItem
+              checked={theme === "light"}
+              onCheckedChange={() => setTheme("light")}
+            >
               <Sun className="mr-2 h-4 w-4" />
               <span>Light</span>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
+            </DropdownMenuCheckboxItem>
+            <DropdownMenuCheckboxItem
+              checked={theme === "dark"}
+              onCheckedChange={() => setTheme("dark")}
+            >
               <Moon className="mr-2 h-4 w-4" />
               <span>Dark</span>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Laptop className="mr-2 h-4 w-4" />
-              <span>System</span>
-            </DropdownMenuItem>
+            </DropdownMenuCheckboxItem>
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
---
+++ src/lib/themer.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export function darkTheme() {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "light");
+
+  const handleClick = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    console.log("theme change");
+
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return { theme, setTheme };
+}
