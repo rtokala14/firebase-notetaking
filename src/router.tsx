f49899057960abd610aee8ebd6231b330592d0d1
Rohit Tokala <rtokala@purdue.edu>
2023-02-06 14:46:48 -0500

    Add routes for protected pages

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,13 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import { BaseLayout } from "./layouts/BaseLayout";
 import LoginPage from "./pages/loginPage";
 import HomePage from "./pages/homePage";
+import { LoggedInLayout } from "./layouts/LoggedInLayout";
+import NotesPage from "./pages/notesPage";
+import ArchivePage from "./pages/archivePage";
+import TrashPage from "./pages/trashPage";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./lib/firebase";
 
 export const router = createBrowserRouter([
   {
@@ -18,8 +24,39 @@ export const router = createBrowserRouter([
             path: "/login",
             element: <LoginPage />,
           },
+          {
+            element: (
+              <RequireAuth>
+                <LoggedInLayout />
+              </RequireAuth>
+            ),
+            children: [
+              {
+                path: "/notes",
+                element: <NotesPage />,
+              },
+              {
+                path: "/archive",
+                element: <ArchivePage />,
+              },
+              {
+                path: "/trash",
+                element: <TrashPage />,
+              },
+            ],
+          },
         ],
       },
     ],
   },
 ]);
+
+function RequireAuth({ children }: { children: JSX.Element }) {
+  const [user] = useAuthState(auth);
+
+  if (!user) {
+    return <Navigate to={"/"} replace />;
+  }
+
+  return children;
+}
