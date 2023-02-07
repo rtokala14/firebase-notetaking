f162d1c4c6142dd30853ec5becfb9b04b3b25e74
Rohit Tokala <rtokala@purdue.edu>
2023-02-07 14:30:10 -0500

    Add basic sidebar styles

--- src/layouts/LoggedInLayout.tsx
+++ src/layouts/LoggedInLayout.tsx
@@ -4,16 +4,28 @@ import { NavLink, Outlet } from "react-router-dom";
 export function LoggedInLayout() {
   return (
     <div className=" w-full min-h-full flex">
-      <div className=" h-full w-14 flex flex-col gap-4 px-2 pt-4">
+      <div className=" h-full w-18 group hover:w-32 flex flex-col gap-4 px-2 pt-4 border-r border-r-slate-200 dark:border-r-slate-700">
         <NavLink
           to={"/notes"}
-          className=" rounded-full p-1 flex flex-col items-center justify-center gap-1 active:bg-slate-400 border-r border-r-slate-200 dark:border-r-slate-700 active:dark:bg-slate-500"
+          className=" rounded-full p-2 flex items-center gap-3 hover:bg-slate-100 dark:hover:bg-slate-800 dark:border-r-slate-700"
         >
-          <BookOpen className=" w-4 h-4" />
-          <span>Notes</span>
+          <BookOpen className=" w-5 h-5" />
+          <span className=" hidden group-hover:block">Notes</span>
         </NavLink>
-        <Archive className=" w-4 h-4" />
-        <Trash className=" w-4 h-4" />
+        <NavLink
+          to={"/archive"}
+          className=" rounded-full p-2 flex  items-center justify-center gap-3 hover:bg-slate-100 dark:hover:bg-slate-800 active:bg-slate-400  dark:border-r-slate-700 active:dark:bg-slate-500"
+        >
+          <Archive className=" w-5 h-5" />
+          <span className=" hidden group-hover:block">Archive</span>
+        </NavLink>
+        <NavLink
+          to={"/trash"}
+          className=" rounded-full p-2 flex  items-center justify-center gap-3 hover:bg-slate-100 dark:hover:bg-slate-800 active:bg-slate-400  dark:border-r-slate-700 active:dark:bg-slate-500"
+        >
+          <Trash className=" w-5 h-5" />
+          <span className=" hidden group-hover:block">Trash</span>
+        </NavLink>
       </div>
       <Outlet />
     </div>
