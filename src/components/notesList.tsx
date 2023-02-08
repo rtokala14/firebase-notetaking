184f37c49ef4ebfff70c5aeacfdc975f10582ac3
Rohit Tokala <rtokala@purdue.edu>
2023-02-08 18:59:32 -0500

    Fetch notes titles for now

--- src/components/notesList.tsx
+++ src/components/notesList.tsx
@@ -1,5 +1,41 @@
+import {
+  type DocumentData,
+  collection,
+  onSnapshot,
+  query,
+} from "firebase/firestore";
+import { auth, db } from "../lib/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect, useState } from "react";
+
 function NotesList() {
-  return <div>NotesList</div>;
+  const [user, loading] = useAuthState(auth);
+  const collectionRef = collection(db, !loading && user ? user.uid : "theVoid");
+  const [notes, setNotes] = useState<DocumentData[]>([]);
+
+  useEffect(() => {
+    const q = query(collectionRef);
+
+    const unsub = onSnapshot(collectionRef, (querySnapshot) => {
+      const items: DocumentData[] = [];
+      querySnapshot.forEach((doc) => {
+        items.push(doc.data());
+      });
+      setNotes(items);
+    });
+
+    return () => {
+      unsub();
+    };
+  }, [loading]);
+
+  return (
+    <div>
+      {notes.map((note) => (
+        <div key={note.id}>{note.title}</div>
+      ))}
+    </div>
+  );
 }
 
 export default NotesList;
