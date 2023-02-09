1d0985e6c3e6d216a31d1a24379c264ba3041ccb
Rohit Tokala <rtokala@purdue.edu>
2023-02-09 00:57:59 -0500

    Add dropdown menu to note

--- src/components/note.tsx
+++ src/components/note.tsx
@@ -1,6 +1,13 @@
 import { type DocumentData, updateDoc, doc } from "firebase/firestore";
-import { Archive, Trash } from "lucide-react";
 import { db } from "../lib/firebase";
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+} from "./ui/dropdown-menu";
+import { Archive, ArrowDown, Trash, Undo } from "lucide-react";
+import { Button } from "./ui/button";
 
 function Note({ noteData }: { noteData: DocumentData }) {
   const ref = doc(db, `/userNotes/${noteData.id}`);
@@ -13,21 +20,44 @@ function Note({ noteData }: { noteData: DocumentData }) {
     const ret = updateDoc(ref, { archive: true, trash: false });
   }
 
+  function undoDelete() {
+    const ret = updateDoc(ref, { archive: false, trash: false });
+  }
+
   return (
     <div className=" relative max-h-[440px] break-inside-avoid mb-4 rounded-md group min-h-[140px] border gap-1 dark:border-slate-700 border-slate-200 flex flex-col items-start p-2 pb-8">
-      <div className=" text-xl font-medium">{noteData.title}</div>
+      <div className=" flex justify-between items-center w-full">
+        <div className=" text-xl font-medium py-2">{noteData.title}</div>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button className=" rounded-full p-3" variant={"ghost"}>
+              <ArrowDown className=" h-4 w-4 " />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem
+              className=" items-center flex"
+              onClick={() => (noteData.trash ? undoDelete() : handleDelete())}
+            >
+              {noteData.trash ? (
+                <Undo className=" mr-2 h-4 w-4" />
+              ) : (
+                <Trash className=" mr-2 h-4 w-4" />
+              )}
+              <span>{noteData.trash ? "Recover note" : "Move to Trash"}</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className=" flex items-center"
+              onClick={() => handleArchive()}
+            >
+              <Archive className=" mr-2 h-4 w-4" />
+              <span>Move to Archive</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
       <hr className=" bg-slate-200 dark:bg-slate-700 w-full h-[1px]" />
       <div className=" overflow-hidden">{noteData.body}</div>
-      <div className=" absolute bottom-2 w-full hidden group-hover:flex gap-2">
-        <Trash
-          className=" h-5 w-5 hover:cursor-pointer text-red-400"
-          onClick={() => handleDelete()}
-        />
-        <Archive
-          className=" h-5 w-5 hover:cursor-pointer "
-          onClick={() => handleArchive()}
-        />
-      </div>
     </div>
   );
 }
