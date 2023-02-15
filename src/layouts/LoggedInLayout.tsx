99aa5ad3b4d41004066a984e1e93aa92538db54d
Rohit Tokala <rtokala@purdue.edu>
2023-02-15 17:58:20 -0500

    Add toggle for opening sidebar

--- src/layouts/LoggedInLayout.tsx
+++ src/layouts/LoggedInLayout.tsx
@@ -1,43 +1,98 @@
-import { Archive, BookOpen, Trash } from "lucide-react";
+import {
+  Archive,
+  BookOpen,
+  SidebarCloseIcon,
+  SidebarOpenIcon,
+  Trash,
+} from "lucide-react";
+import { useState } from "react";
 import { NavLink, Outlet } from "react-router-dom";
+import { cn } from "../lib/utils";
 
 export function LoggedInLayout() {
+  const [sideOpen, setIsSideOpen] = useState(false);
   return (
     <div className=" w-full min-h-full flex">
       {/* Sidebar */}
-      <div className=" animate-in fade-in-20 duration-1000 slide-in-from-left-2 h-[93vh] w-18 group hover:w-32 flex flex-col items-center hover:items-start gap-4 px-2 hover:px-0 pt-4 border-r border-r-slate-200 dark:border-r-slate-800 transition-width ease-in-out">
+      <div
+        className={cn(
+          " h-[93vh] w-18 group flex flex-col items-center gap-4 px-2  pt-4 border-r border-r-slate-200 dark:border-r-slate-800 transition-width duration-700 delay-500",
+          sideOpen && "w-36 px-0"
+        )}
+      >
+        <div
+          className={cn(
+            !sideOpen && "w-full flex items-center justify-center ",
+            sideOpen && "flex items-center w-full justify-end pr-2"
+          )}
+        >
+          {sideOpen ? (
+            <div
+              onClick={() => setIsSideOpen(false)}
+              className="hover:cursor-pointer rounded-full p-2 hover:bg-slate-200 dark:hover:bg-slate-800"
+            >
+              <SidebarCloseIcon className=" h-5 w-5 " />
+            </div>
+          ) : (
+            <div
+              onClick={() => setIsSideOpen(true)}
+              className="hover:cursor-pointer rounded-full p-2 hover:bg-slate-200 dark:hover:bg-slate-800"
+            >
+              <SidebarOpenIcon className=" h-5 w-5 " />
+            </div>
+          )}
+        </div>
+        <hr className={cn(" text-slate-200 w-full border")} />
         <NavLink
           to={"/notes"}
           className={({ isActive }) =>
             isActive
-              ? "rounded-full group-hover:rounded-l-none p-2 w-full flex items-center bg-slate-300 dark:bg-slate-700 gap-3 hover:bg-slate-200 dark:hover:bg-slate-800 dark:border-r-slate-700"
-              : "rounded-full group-hover:rounded-l-none p-2 w-full flex items-center gap-3 hover:bg-slate-100 dark:hover:bg-slate-800 dark:border-r-slate-700"
+              ? cn(
+                  "rounded-full p-2 w-full flex items-center bg-slate-200 dark:bg-slate-700 gap-3 hover:bg-slate-100 dark:hover:bg-slate-800 dark:border-r-slate-700",
+                  sideOpen && "rounded-l-none"
+                )
+              : cn(
+                  "rounded-full p-2 w-full flex items-center gap-3 hover:bg-slate-100 dark:hover:bg-slate-800 dark:border-r-slate-700",
+                  sideOpen && "rounded-l-none"
+                )
           }
         >
-          <BookOpen className=" group-hover:ml-2 w-5 h-5" />
-          <span className=" hidden group-hover:block">Notes</span>
+          <BookOpen className=" w-5 h-5" />
+          <span className={cn(" hidden ", sideOpen && "block")}>Notes</span>
         </NavLink>
         <NavLink
           to={"/archive"}
           className={({ isActive }) =>
             isActive
-              ? "rounded-full group-hover:rounded-l-none p-2 w-full flex items-center bg-slate-300 dark:bg-slate-700 gap-3 hover:bg-slate-200 dark:hover:bg-slate-800 dark:border-r-slate-700"
-              : "rounded-full group-hover:rounded-l-none p-2 w-full flex items-center gap-3 hover:bg-slate-100 dark:hover:bg-slate-800 dark:border-r-slate-700"
+              ? cn(
+                  "rounded-full p-2 w-full flex items-center bg-slate-200 dark:bg-slate-700 gap-3 hover:bg-slate-100 dark:hover:bg-slate-800 dark:border-r-slate-700",
+                  sideOpen && "rounded-l-none"
+                )
+              : cn(
+                  "rounded-full p-2 w-full flex items-center gap-3 hover:bg-slate-100 dark:hover:bg-slate-800 dark:border-r-slate-700",
+                  sideOpen && "rounded-l-none"
+                )
           }
         >
-          <Archive className=" group-hover:ml-2 w-5 h-5" />
-          <span className=" hidden group-hover:block">Archive</span>
+          <Archive className=" w-5 h-5" />
+          <span className={cn(" hidden ", sideOpen && "block")}>Archive</span>
         </NavLink>
         <NavLink
           to={"/trash"}
           className={({ isActive }) =>
             isActive
-              ? "rounded-full group-hover:rounded-l-none p-2 w-full flex items-center bg-slate-300 dark:bg-slate-700 gap-3 hover:bg-slate-200 dark:hover:bg-slate-800 dark:border-r-slate-700"
-              : "rounded-full group-hover:rounded-l-none p-2 w-full flex items-center gap-3 hover:bg-slate-100 dark:hover:bg-slate-800 dark:border-r-slate-700"
+              ? cn(
+                  "rounded-full p-2 w-full flex items-center bg-slate-200 dark:bg-slate-700 gap-3 hover:bg-slate-100 dark:hover:bg-slate-800 dark:border-r-slate-700",
+                  sideOpen && "rounded-l-none"
+                )
+              : cn(
+                  "rounded-full p-2 w-full flex items-center gap-3 hover:bg-slate-100 dark:hover:bg-slate-800 dark:border-r-slate-700",
+                  sideOpen && "rounded-l-none"
+                )
           }
         >
-          <Trash className=" group-hover:ml-2 w-5 h-5" />
-          <span className=" hidden group-hover:block">Trash</span>
+          <Trash className=" w-5 h-5" />
+          <span className={cn(" hidden ", sideOpen && "block")}>Trash</span>
         </NavLink>
       </div>
       <Outlet />
