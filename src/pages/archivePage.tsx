58bc415b073c8d4bbcd08b4e0c79f5b55c10a3f4
Rohit Tokala <rtokala@purdue.edu>
2023-02-15 01:28:44 -0500

    Add page titles for Archive and Trash

--- src/pages/archivePage.tsx
+++ src/pages/archivePage.tsx
@@ -3,6 +3,7 @@ import ArchiveList from "../components/archiveList";
 function ArchivePage() {
   return (
     <div className=" w-full gap-7 flex flex-col items-center pt-10">
+      <h2 className=" text-xl font-bold self-start ml-4">Archive</h2>
       <ArchiveList />
     </div>
   );
--- src/pages/trashPage.tsx
+++ src/pages/trashPage.tsx
@@ -3,6 +3,8 @@ import TrashList from "../components/trashList";
 function TrashPage() {
   return (
     <div className=" w-full gap-7 flex flex-col items-center pt-10">
+      <h2 className=" text-xl font-bold self-start ml-4">Trash</h2>
+
       <TrashList />
     </div>
   );
